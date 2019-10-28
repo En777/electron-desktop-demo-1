b4e75ac9effc39bd7daf0b6f9f2e1515a2bb82e4
En <En>
2019-10-28 23:30:23 +0800

    FS

---
+++ src/renderer/views/Demos/FS.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>File System</h1>
+
+    <div>
+      <ol>
+        <li v-for="(file) in list" :key="file">
+          <small>{{ file }}</small>
+        </li>
+      </ol>
+    </div>
+  </div>
+</template>
+
+<script>
+  const fs = require('fs')
+
+  export default {
+    data () {
+      return {
+        list: []
+      }
+    },
+    methods: {
+      getFileList () {
+        this.list = fs.readdirSync('/')
+      }
+    },
+    created () {
+      this.getFileList()
+    },
+  }
+</script>
+
+<style lang="scss">
+
+</style>
