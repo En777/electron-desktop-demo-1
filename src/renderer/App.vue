f4034c69d2ff6f2fa7e32f8a3f26b9aea386d2a5
En <En>
2019-10-28 21:21:50 +0800

    test vue file

---
+++ src/renderer/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app" class="appContainer">
+    <h1>Electron and Vue</h1>
+
+  </div>
+</template>
+
+<script>
+// import { remote } from 'electron'
+
+export default {
+  methods: {
+
+  },
+  created () {
+
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
