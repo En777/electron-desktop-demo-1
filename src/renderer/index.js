26ff39ed210a12fef25402436aca7de5aa3b9b26
En <En>
2019-10-28 21:31:23 +0800

    router file

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App'
+import router from './router'
 
 process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = 'true'
 
 // eslint-disable-next-line
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
